1dcb7eab9879a6bd691addfb6d8bba5dc80ae5b9
jingshuai <jin.gshuai@qq.com>
2018-04-05 23:12:13 +0800

    修复列表的删除功能

--- src/main/java/com/jing/experiment/news/controller/NewsController.java
+++ src/main/java/com/jing/experiment/news/controller/NewsController.java
@@ -1,33 +1,25 @@
 package com.jing.experiment.news.controller;
 
-import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.jing.experiment.news.bean.NewsBean;
-import com.jing.experiment.news.mapper.NewsMapper;
 import com.jing.experiment.news.bean.PageInfo;
+import com.jing.experiment.news.mapper.NewsMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import tk.mybatis.mapper.common.Mapper;
 
 import javax.annotation.Resource;
-import javax.print.Doc;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
 /**
- * @author jings
+ * @ author jings
  * ����ģ��
  */
 @RestController
@@ -45,55 +37,60 @@ public class NewsController {
 
 
     /**
-     * @return
-     * @Description ������ѯ����
+     * @return pageinfo
+     * @ Description ������ѯ����
      * @author jingshuai
-     * @Date 2017��12��7��
+     * @param newsBean
+     * @ Date 2017��12��7��
      */
     @RequestMapping(value = "/queryNewsByCondition", method = RequestMethod.GET)
     public PageInfo<NewsBean> queryNewsByCondition(NewsBean newsBean) {
 
         //mapper.selectByRowBounds(record, rowBounds)
-        List<NewsBean> newsList = new ArrayList<>();
+        List<NewsBean> newsList ;
         PageHelper.offsetPage(newsBean.getOffset(), newsBean.getPageSize());
         //PageHelper.startPage(newsBean.getOffset(),20);
         //newsList   = newsMapper.queryNewsByCondition(newsBean);
-        newsBean.setCategoryid(1);
+        //newsBean.setCategoryid(1);
         newsList = newsMapper.queryNewsByCondition(newsBean);
         // newsMapper.queryNewsByCondition(newsBean);
-        log.debug(newsList.toString());
-        PageInfo<NewsBean> pageInfo = new PageInfo<NewsBean>(newsList);
-        return pageInfo;
+        //log.debug(newsList.toString());
+        //PageInfo<NewsBean> pageInfo = ;
+        return new PageInfo<>(newsList);
 
     }
 
-    /**
-     * @param NewsId
-     * @return
-     * @Description ����id��ѯ����
-     * @author jingshuai
-     * @Date 2017��12��7��
-     */
-//    @ResponseBody
-//    @RequestMapping(value = "/queryNewsById", method = RequestMethod.GET)
-//    public String queryNewsById(@RequestParam("newsId") String NewsId) {
-//        log.info("into queryNewsById method!!!");
-////		if(StringUtils.isEmpty(NewsId)){
-////			throw new IllegalArgumentException("����ID�ǿյ�");
-////		}
-//        return newsMapper.queryNewsById(NewsId).toString();
-//    }
     public NewsBean updateNewsById() {
 
         return newsMapper.updateNewsById();
     }
 
     /**
+     * @param id
+     * @ Description: 根据id删除对应的新闻
+     * @author jingShuai
+     * @ Date 2018/4/5 13:37
+     */
+    @RequestMapping(value = "news", method = RequestMethod.GET)
+    public void delNews(String id) {
+
+        log.info("进入删除方法，id ："+id);
+        if(id == null || id.equals("")) {
+            throw new NullPointerException("id 为空") ;
+        }
+        int status = newsMapper.deleteByPrimaryKey(id);
+        if(status == 0) {
+            log.info("该新闻已删除");
+        }
+        //newsMapper.deleteByIds(id);
+    }
+
+    /**
      * @param newsBean
-     * @Description �������
+     * @ Description �������
      * @author jingshuai
-     * @Date 2017��12��7��
-     * @retur
+     * @ Date 2017��12��7��
+     * @return string
      */
     @RequestMapping("/addNews")
     public String addNews(NewsBean newsBean, @RequestParam("file") MultipartFile multipartFile) {
@@ -112,13 +109,12 @@ public class NewsController {
             String path = uploadDir;
             System.out.println("--------" + path);
             File file = new File(path, name);
-            if (!file.getParentFile().exists()) {
-                file.getParentFile().mkdirs();
-            }
+//            if (!file.getParentFile().exists()) {
+//                file.getParentFile().mkdirs();
+//            }
             try {
                 multipartFile.transferTo(new File(path + File.separator + name));
             } catch (IllegalStateException | IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
@@ -130,46 +126,13 @@ public class NewsController {
     }
 
 
-    /**
-     *
-     * @param id
-     * @return
-     */
-    /**
-     * @param
-     * @author jingShuai
-     * @Date 2018/3/23 16:04
-     * @Description:
-     */
-    @RequestMapping(value = "/new", method = RequestMethod.DELETE)
-    public String delNews(String id) {
-        Pattern pattern = Pattern.compile("[0-9]*");
 
-        if (null != id) {
-            System.out.println(id);
-//		  //ͨ��������ʽ�ж��Ƿ�������
-//	         Matcher isNum = pattern.matcher(id);
-//	         int idIn = 0;
-//			 //Ϊtrue,˵��������
-//			 if( isNum.matches() ){
-//				 idIn = Integer.parseInt(id);
-//				 
-//				 mapper.deleteByPrimaryKey(idIn);
-//			 } else {
-//				 return "id��������";
-//			 }
-        } else {
-
-            return "idΪ��";
-        }
-        return "0000";
-    }
-
-    /**
-     * 获取所有的新闻，通过泛型的mapper获取
-     * @return pageinfo
-     *
-     */
+//
+//    /**
+//     * 获取所有的新闻，通过泛型的mapper获取
+//     * @return pageinfo
+//     *
+//     */
 //    public PageInfo<NewsBean> queryAllNews(){
 //
 //        PageHelper.startPage(3,20);
